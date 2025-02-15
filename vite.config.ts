10a427054877da6e845c7139ab7a4d455563d003
Pawan608 <agarwalpawan6089@gmail.com>
2025-02-15 15:53:31 +0530

    fix:vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,26 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    proxy: {
-      '/api/v1': {
-        target: 'http://127.0.0.1:3000',
-        changeOrigin: true,
-        // No rewrite needed as we want to keep /api/v1 in the path
+export default defineConfig(({ mode }) => {
+  // Load environment variables
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
       },
     },
-  },
+    server: {
+      proxy: {
+        '/api/v1': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
 });
